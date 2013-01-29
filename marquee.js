eb261937442e76f38ecaaf2e207e75800e5d997f
Scott Johnson <7.scott.j@gmail.com>
2013-01-29 09:12:16 -0600

    did not keep up with chages, this is what I have

--- marquee.js
+++ marquee.js
@@ -2,17 +2,51 @@
  * @arthur Scott Johnson 2012
 */ 
 
+var currentPanel = 1;
+var totalPanels = 0;
+var autoPlay = true;
+var timePassed = 0;
+var timeToChange = 5;
+
+function autoAdvance(){
+  if(window.timePassed == window.timeToChange ){
+    window.timePassed =0;
+    if(window.currentPanel == window.totalPanels ){
+      window.currentPanel = 0;
+    }
+    if(window.autoPlay == true){
+      $('.marquee_nav a.marquee_nav_item:nth-child('+(window.currentPanel + 1)+')').trigger('click');
+    }
+  }else{
+    window.timePassed += 1;
+  }
+  // /* degug */ $('.autoPlay').html('autoPlay = ' +window.autoPlay);
+  // /* degug */ $('.timePassed').html('timePassed = ' +window.timePassed);
+};
 
 $(document).ready(function() {
+  // /* degug */ $('.autoPlay').html('autoPlay = ' +window.autoPlay);
+  // /* degug */ $('.timePassed').html('timePassed = ' +window.timePassed);
+  // /* degug */ $('.timeToChange').html('timeToChange = ' +window.timeToChange);
+  // /* degug */ $('.currentPanel').html('currentPanel = ' +window.currentPanel);
+  
+  setInterval(autoAdvance, 1000);
+  
+  $('.marquee_container').hover(
+    function(){
+      // roll over
+      window.autoPlay = false;
+      $(this).removeClass('auto_play');
+    },
+    function(){
+      // roll out
+      window.autoPlay = true;
+      window.timePassed = 0;
+      $(this).addClass('auto_play');
+    }
+  );
+  
   // making one long virtual image [1][2][n]
-  // $('img.marquee_panel_photo').each(function(index){
-  //   var photoWidth = $('.marquee_container').width();
-  //   var photoPosition = index * photoWidth;
-  //   
-  //   $('.marquee_content').append('<img class="marquee_photo" style="left:'+photoPosition + ';" src="'+ $(this).attr('src') +'" alt="' + $(this).attr('alt') + '" width="400" height="250" />');
-  //   $('.marquee_content').css('width', photoPosition + photoWidth);
-  //   
-  // });
   $('.marquee_panel_content').each(function(index){
     var panelWidth = $('.marquee_container').width();
     var panelPosition = panelWidth * index;
@@ -24,7 +58,9 @@ $(document).ready(function() {
   
   // add in the nav elements
   $('.marquee_panels .marquee_panel').each(function(index){
-    $('.marquee_nav').append('<a class="marquee_nav_item"></a>');
+    $('.marquee_nav_buttons').append('<a class="marquee_nav_item"></a>');
+    window.totalPanels = index + 1;
+    // /* degug */ $('.totalPanels').html('totalPanels = ' +window.totalPanels);
   });
   
   // manage the click event for the nav elements
@@ -32,28 +68,26 @@ $(document).ready(function() {
     $('.marquee_nav a.marquee_nav_item').removeClass('selected');
     $(this).addClass('selected');
     
-    // calculate where to put the virtual image [1][2][n] so the correct image is in the window
+    // calculate where to put the virtual panel [1][2][n] so the correct panel is in the window
     var navClicked = $(this).index();
     var marqueeWidth = $('.marquee_container').width();
     var distanceToMove = marqueeWidth*(-1);
-    var newPhotoPosition = navClicked * distanceToMove + 'px'
-    var newCaption = $('.marquee_panel_caption').get(navClicked);
+    var newPanelPosition = navClicked * distanceToMove + 'px'
+    window.currentPanel = navClicked +1;
+    // /* degug */ $('.currentPanel').html('currentPanel = ' +window.currentPanel);
     
-    // animate and display the correct image
-    $('.marquee_content').animate({left:newPhotoPosition}, 1000);
+    // animate and display the correct panel
+    $('.marquee_content').animate({left:newPanelPosition}, 1000);
     
-    $('.marquee_caption').animate({top:'212px'}, 500, function(){
-      var newHTML = $(newCaption).html();
-      $('.marquee_caption_content').html(newHTML);
-      setCaption();
-    });
+    // set READ link to current article
+    var currentPanel = $('.marquee_panel_content').get(navClicked);
+    var navLink = $(currentPanel).find('.data').attr('href');
+    $('a#news_current_nav_link').attr('href', navLink);
+    
     
   });
   
-  // wait until all images are loaded to initialize
-  $('.marquee_panels img').imgpreload(function(){
-    initializeMarquee();
-  });
+  initializeMarquee();
 });
 
 // have the marquee start out with no images, then have the first one fade in and be selected
@@ -63,15 +97,15 @@ function initializeMarquee(){
   );
   $('.marquee_nav a.marquee_nav_item:first').addClass('selected');
   $('.marquee_content').fadeIn(500); 
-  setCaption(); 
+  setLink(); 
 }
 
 // get the caption to animate and show on the inital update
-function setCaption(){
-  var captionHeight = $('.marquee_caption').height();
-  var marqueeHeight = $('.marquee_container').height();
-  var newCaptionTop = marqueeHeight - captionHeight - 15;
-  $('.marquee_caption').delay(100).animate({top:newCaptionTop}, 500);
+function setLink(){
+  // set READ link to first article
+  var currentPanel = $('.marquee_panel_content:first');
+  var navLink = $(currentPanel).find('.data').attr('href');
+  $('a#news_current_nav_link').attr('href', navLink);
   
 }
 
